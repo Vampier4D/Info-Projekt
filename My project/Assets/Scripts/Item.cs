740aeb5b3495adae767d6f476f8e524ffe8bfe8a
snaXRuS <daz.news.9e@gmail.com>
2024-09-11 17:05:37 +0200

    Inventory

---
+++ My project/Assets/Scripts/Item.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item { 
+
+    public enum ItemType {
+        EarRing0,
+        EarRing1,
+        EarRing2,
+        EarRing3,
+        EarRing4,
+        EarRing5,
+        EarRing6,
+        EarRing7,
+        EarRing8,
+        EarRing9,
+        EarRing10,
+        EarRing11,
+        HealthPotion,
+        ManaPotion,
+        Key,
+        Coin,
+    }
+    public ItemType Type;
+    public int amount;
+
+
+
+}
+
+  
