b965f3eaa3ace3d3a442b624a68d01f04f9694fe
snaXRuS <daz.news.9e@gmail.com>
2025-01-14 20:18:37 +0100

    fix

--- My project/Assets/Scripts/worldGenPerlin.cs
+++ My project/Assets/Scripts/worldGenPerlin.cs
@@ -4,50 +4,54 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using Unity.Mathematics;
 
-public class WorldGenPerlin : MonoBehaviour
+public class WorldGeneratorPerlin : MonoBehaviour
 {
-    [SerializeField] private int width, plusX, plusY;
+    [SerializeField] private int width;
+    [SerializeField] private bool generateLeft;
+    [SerializeField] private int plusY;
+    [SerializeField] private int plusX = 0;
     [SerializeField] private bool left;
     [SerializeField] private Tilemap dirtTilemap, grassTilemap, stoneTilemap;
-    [SerializeField] private TileBase dirtTile, grassTile, stoneTile;
-    [Range(0,100)]
-    [SerializeField] private float heightValue,smoothness;
-void Start()
-{
-    GenerateWorld();
-}
-
-private void GenerateWorld()
-{
-
-    if (left == true)
+    [SerializeField] private TileBase dirtTile;
+    [SerializeField] private TileBase grassTile;
+    [SerializeField] private TileBase stoneTile;
+    [Range(0, 100)]
+    [SerializeField] private float heightValue, smoothness;
+    void Start()
     {
-        for (int x = 0; x < width; x++)
+        GenerateWorld();
+    }
+
+    private void GenerateWorld()
+    {
+        if (generateLeft == true)
         {
-            
-            int height = Mathf.RoundToInt(heightValue* Mathf.PerlinNoise(x/smoothness, 0));
-            for (int y = 0; y < height; y++)
+            if (left == true)
+
+                for (int x = 0; x < width; x++)
+                {
+                    generator(x);
+                }
+        }
+        else
+        {
+            for (int x = 0; x > width; x--)
             {
-
-                stoneTilemap.SetTile(new Vector3Int(x + plusX, y + plusY, 0), stoneTile);
+                generator(x);
             }
-            grassTilemap.SetTile(new Vector3Int(x + plusX, height + plusY, 0), grassTile);
-            dirtTilemap.SetTile(new Vector3Int(x + plusX, height + plusY - 1, 0), dirtTile);
-
         }
     }
-    else if (left == true)
+    private void generator(int x)
+{
+    float perlinValue = Mathf.PerlinNoise(x / smoothness, 0);
+    int height = Mathf.RoundToInt(heightValue * perlinValue);
+    for (int y = 0; y < height; y++)
     {
-        for (int x = 0; x > width; x--)
-        {
-       int height = Mathf.RoundToInt(heightValue* Mathf.PerlinNoise(x/smoothness, 0));
-            for (int y = 0; y < height; y++)
-            {
-                stoneTilemap.SetTile(new Vector3Int(x + plusX, y + plusY, 0), stoneTile);
-            }
-            grassTilemap.SetTile(new Vector3Int(x + plusX, height + plusY, 0), grassTile);
-            dirtTilemap.SetTile(new Vector3Int(x + plusX, height + plusY - 1, 0), dirtTile);
-        }
+        stoneTilemap.SetTile(new Vector3Int(x + plusX, y + plusY, 0), stoneTile);
     }
+    grassTilemap.SetTile(new Vector3Int(x + plusX, height + plusY, 0), grassTile);
+    dirtTilemap.SetTile(new Vector3Int(x + plusX, height + plusY - 1, 0), dirtTile);
 }
 }
+
+
